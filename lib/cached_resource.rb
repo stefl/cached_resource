3c86e23f1f37c5740bb7de1f302e51bf50391780
dmcinnes <dmcinnes@a06862d0-bf29-e17f-e302-fbc15f989f17>
2008-01-31 22:29:21 +0000

    lil cleanup, now if hits server for list will not hit for every element
    git-svn-id: http://svn.latimesdev.com/svn/repos/shared/ruby/plugins/cached_resource/trunk@3105 a06862d0-bf29-e17f-e302-fbc15f989f17

--- lib/cached_resource.rb
+++ lib/cached_resource.rb
@@ -1,18 +1,26 @@
 module CachedResource
+
   def self.included(receiver)
     receiver.extend(ClassMethods)
   end
+
   module ClassMethods
+
     def cached_resource(options = {})
+
       acts_as_cached(options)
+
       class << self
+
         def find_with_cache(*arguments)
-          scope                         = arguments.slice!(0)
-          options                       = arguments.slice!(0) || {}
+          scope = arguments.slice!(0)
+          options = arguments.slice!(0) || {}
           case scope
           when :all   then cached_find_every(options)
           when :first then cached_find_every(options).first
-            # when :one   then cached_find_one(options)
+          # TODO should this actually cache something?
+          # when :one   then cached_find_one(options)
+          when :one   then find_without_cache(arguments)
           else             cached_find_single(scope)
           end
         end
@@ -20,30 +28,35 @@ module CachedResource
         def cached_find_every(options)
           prefix_options, query_options = split_options(options[:params])
           path_as_key = collection_path(prefix_options, query_options)
+          response_elements = nil
           cached_id_array = get_cache(path_as_key) do
-            find_without_cache(:all,:params=>query_options).collect do |cached_element|
-              logger.info "====================================================="
-              logger.info "This cached by cached_resource plugin"
-              logger.info "key    : #{cached_element.id}"
-              logger.info "value  : #{cached_element}"
-              logger.info "====================================================="
+            response_elements = find_without_cache(:all,:params=>query_options)
+            response_elements.collect do |cached_element|
+              #logger.debug "====================================================="
+              #logger.debug "This cached by cached_resource plugin"
+              #logger.debug "key    : #{cached_element.id}"
+              #logger.debug "value  : #{cached_element}"
+              #logger.debug "====================================================="
               set_cache(cached_element.id,cached_element,self.cache_config[:ttl]) 
               cached_element.id
             end  
           end
-          logger.info "====================================================="
-          logger.info "This cached by cached_resource plugin"
-          logger.info "key    : #{path_as_key}"
-          logger.info "value  : [#{cached_id_array.join(',')}]"
-          logger.info "====================================================="
-          get_cache(cached_id_array).values
+          #logger.debug "====================================================="
+          #logger.debug "This cached by cached_resource plugin"
+          #logger.debug "key    : #{path_as_key}"
+          #logger.debug "value  : [#{cached_id_array.join(',')}]"
+          #logger.debug "====================================================="
+          response_elements || get_cache(cached_id_array).values
         end
+
         def cached_find_single(scope)
           get_cache(scope) do
             self.find_without_cache(scope)
           end
         end
+
         alias_method_chain :find, :cache
+
       end
     end
   end
