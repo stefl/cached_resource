5b3578daa0fb4d452dab55adf79cb19072fbfa5a
srao <srao@a06862d0-bf29-e17f-e302-fbc15f989f17>
2008-01-31 21:51:16 +0000

    git-svn-id: http://svn.latimesdev.com/svn/repos/shared/ruby/plugins/cached_resource/trunk@3104 a06862d0-bf29-e17f-e302-fbc15f989f17

--- lib/cached_resource.rb
+++ lib/cached_resource.rb
@@ -1,17 +1,10 @@
-# ActsAsCachedResource
-
 module CachedResource
-    
   def self.included(receiver)
     receiver.extend(ClassMethods)
   end
-
   module ClassMethods
-    
     def cached_resource(options = {})
-
       acts_as_cached(options)
-
       class << self
         def find_with_cache(*arguments)
           scope                         = arguments.slice!(0)
@@ -26,29 +19,32 @@ module CachedResource
 
         def cached_find_every(options)
           prefix_options, query_options = split_options(options[:params])
-          path                          = collection_path(prefix_options, query_options)
-          url                           = get_cache(path) do
-            elements                    = find_without_cache(:all,:params=>query_options)
-            output                      = []
-            elements.each do |element|
-              set_cache(element.id, element) unless fetch_cache(element.id)
-              output << element.id
-            end
-            output.join(',')
+          path_as_key = collection_path(prefix_options, query_options)
+          cached_id_array = get_cache(path_as_key) do
+            find_without_cache(:all,:params=>query_options).collect do |cached_element|
+              logger.info "====================================================="
+              logger.info "This cached by cached_resource plugin"
+              logger.info "key    : #{cached_element.id}"
+              logger.info "value  : #{cached_element}"
+              logger.info "====================================================="
+              set_cache(cached_element.id,cached_element,self.cache_config[:ttl]) 
+              cached_element.id
+            end  
           end
-          url.split(',').collect{|photo| get_cache(photo)}
+          logger.info "====================================================="
+          logger.info "This cached by cached_resource plugin"
+          logger.info "key    : #{path_as_key}"
+          logger.info "value  : [#{cached_id_array.join(',')}]"
+          logger.info "====================================================="
+          get_cache(cached_id_array).values
         end
-
         def cached_find_single(scope)
           get_cache(scope) do
             self.find_without_cache(scope)
           end
         end
-
         alias_method_chain :find, :cache
-
       end
-
     end
   end
 end
