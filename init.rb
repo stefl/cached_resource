28685812236e46e9d56dfb660542d9191995f903
dmcinnes <dmcinnes@a06862d0-bf29-e17f-e302-fbc15f989f17>
2008-01-31 20:01:59 +0000

    added a check to see if cache_fu is loaded
    git-svn-id: http://svn.latimesdev.com/svn/repos/shared/ruby/plugins/cached_resource/trunk@3102 a06862d0-bf29-e17f-e302-fbc15f989f17

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
-# Include hook code here
+
+# check to see if cache_fu is loaded
+puts "=> cached_resource requires cache_fu! >> http://svn.latimesdev.com/repos/shared/ruby/plugins/cache_fu/" unless Object.const_defined?(:ActsAsCached)
 
 ActiveResource::Base.send :include, CachedResource
