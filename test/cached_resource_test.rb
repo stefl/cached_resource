ced2aae4b02d01bb5259cb380f41070d339e9f94
dmcinnes <dmcinnes@a06862d0-bf29-e17f-e302-fbc15f989f17>
2008-02-01 22:06:57 +0000

    added unit test
    git-svn-id: http://svn.latimesdev.com/svn/repos/shared/ruby/plugins/cached_resource/trunk@3157 a06862d0-bf29-e17f-e302-fbc15f989f17

--- test/cached_resource_test.rb
+++ test/cached_resource_test.rb
@@ -1,8 +1,31 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+
+ENV['RAILS_ENV'] = 'test'
+
 require 'test/unit'
 
-class ActsAsCachedResourceTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
+
+module ActsAsCached
+  module ClassMethods
+    def get_cache
+      yield if block_given?
+    end
+    def set_cache
+    end
   end
+end
+
+class TestClass < ActiveResource::Base
+  cached_resource
+end
+
+class CachedResourceTest < Test::Unit::TestCase
+
+  def test_load
+    assert TestClass.respond_to?(:cached_resource, "should respond to cached_resource")
+    assert TestClass.respond_to?(:find_with_cache, "should respond to find_with_cache")
+    assert TestClass.respond_to?(:find_without_cache, "should respond to find_without_cache")
+  end
+
 end
